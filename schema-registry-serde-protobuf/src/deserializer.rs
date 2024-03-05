a5257d52ae733afa7fa50e56711e5571d38fbb5f
Guilherme Favero Ferreira <guifaveroferreira@gmail.com>
2024-03-05 17:06:33 -0300

    proceed with boilerplate for protobuf

--- schema-registry-serde-protobuf/src/deserializer.rs
+++ schema-registry-serde-protobuf/src/deserializer.rs
@@ -1,1 +1,23 @@
-pub struct SchemaRegistryProtoDeserializer {}
+use std::sync::Arc;
+
+use serde::de::DeserializeOwned;
+
+use schema_registry_client::SchemaRegistryClient;
+use schema_registry_serde::SchemaRegistryDeserializer;
+
+use crate::error::ProtoDeserializationError;
+
+pub struct SchemaRegistryProtoDeserializer {
+    schema_registry_client: Arc<dyn SchemaRegistryClient>,
+}
+
+impl SchemaRegistryDeserializer for SchemaRegistryProtoDeserializer {
+    type Error = ProtoDeserializationError;
+
+    async fn deserialize<T>(&self, data: Option<&[u8]>) -> Result<T, Self::Error>
+    where
+        T: DeserializeOwned,
+    {
+        unimplemented!()
+    }
+}
--- schema-registry-serde-protobuf/src/serializer.rs
+++ schema-registry-serde-protobuf/src/serializer.rs
@@ -1,1 +1,35 @@
+use async_trait::async_trait;
+use serde::Serialize;
+
+use schema_registry_serde::{SchemaRegistrySerializer, SubjectNameStrategy};
+
+use crate::error::ProtoSerializationError;
+
 pub struct SchemaRegistryProtoSerializer {}
+
+#[async_trait]
+impl SchemaRegistrySerializer for SchemaRegistryProtoSerializer {
+    type Error = ProtoSerializationError;
+
+    async fn serialize_value<T>(
+        &self,
+        strategy: SubjectNameStrategy<'_>,
+        data: &T,
+    ) -> Result<Vec<u8>, Self::Error>
+    where
+        T: Serialize + Send + Sync,
+    {
+        unimplemented!()
+    }
+
+    async fn serialize_key<T>(
+        &self,
+        strategy: SubjectNameStrategy<'_>,
+        data: &T,
+    ) -> Result<Vec<u8>, Self::Error>
+    where
+        T: Serialize + Send + Sync,
+    {
+        unimplemented!()
+    }
+}
